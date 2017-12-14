e1ea5d914cf612010deb16c07c68d788a214b6f1
Evgeniya <jenyapawko@gmail.com>
2017-12-14 15:27:34 +0200

    add changes

--- react-basics/src/Directory.js
+++ react-basics/src/Directory.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import Folder from './Folder';
 import Document from './Document';
 
 
@@ -35,56 +34,3 @@ class Directory extends Component {
     }
 }
 export default Directory;
-        
-            // <div>
-            //   {
-            //    this.props.data && this.props.data.map((node, id) => {
-            //     if (node.type === 'root'){
-            //         let value = 'root';
-            //         return <Directory key = {id} value = {value} data = {node.data} />
-            //     } else if (node.type === 'dir'){
-            //         return <Directory key = {id} value = {node.name} data = {node.data} />
-            //     } else {
-            //         return<Document key = {id} value = {node.name} />
-            //     }
-            //    })
-            //   }
-            // </div>
-
-        //return (
-                //<div>
-                    //{this.props.data.map((node, id) =>{
-                       // if(node.type === 'root'){
-                         //   let name = 'root';
-                           // return <Folder key = {id} name = {name} data = {node.data}/>
-                        //}
-                        //if(node.type === 'dir'){
-                          //  return <Folder key = {id} name = {node.name} data = {node.data}/>
-                        //}
-                        //if(node.type === 'file' || node.type === 'exec'){
-                          //  return <Document key = {id} name = {node.name} />
-                        //}
-                    //})}   
-                //</div>
-       // )
-  //  }
-
-
-
-
-  
-    // renderView(data) {
-    //     {
-    //         this.props.data && this.props.data.map((node, id) => {
-
-    //             return (
-    //                 <Directory key={id} data={node.data} name={node.name} />
-    //             );
-    //         })
-    //     }
-    // }
-                           // {
-                             //  this.props.data.map((node, id) => {
-                               //     return <Document key = {id} name = {node.name} data = {node.data} />
-                               //})
-                            //}
